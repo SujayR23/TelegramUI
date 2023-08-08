50551422ca8a13d23e632c7e259433feaa056842
SujayR23 <138759410+SujayR23@users.noreply.github.com>
2023-08-08 15:14:14 +0530

    Update main.dart

--- dice_roller/main.dart
+++ dice_roller/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dice ROller',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: HomePage(),
+    );
+  }
+}
 
